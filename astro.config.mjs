da2b60f627c596db5be3b117ce8da1dabfda33df
dPanibra0 <dpanibra0@gmail.com>
2026-03-27 19:23:05 -0500

    feat: add sitemap support and improve SEO metadata
    - Added @astrojs/sitemap dependency for sitemap generation.
    - Introduced robots.txt to allow search engine indexing and specify sitemap location.
    - Added Open Graph and Twitter Card metadata in MainLayout.astro for better social sharing.
    - Included a new social image for Open Graph in the public/images directory.
    - Updated Experience.astro to enhance spacing and adjust reveal behavior based on screen width.
    - Modified Hero.astro to use globalThis for setTimeout.
    - Added TypeScript as a dev dependency for better type checking.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://davidpanibra.dev",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()],
 });
